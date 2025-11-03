1da4c8ebeb5a2e1db6cd13f1ca0ee07bff7a70ee
peaceapple <astonreba@gmail.com>
2025-11-03 12:51:58 +0300

    Add login form

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginCredentials } from './loginInterface';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  credentials: LoginCredentials = {
+    email: '',
+    password: ''
+  };
 
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('I am logged in---->', this.credentials)
+  }
 }
