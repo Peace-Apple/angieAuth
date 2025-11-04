8d829a963c826490814e6bb899dcd1be2e1ad9c4
peaceapple <astonreba@gmail.com>
2025-11-04 22:01:26 +0300

    Work on reusable auth form

--- src/app/auth-form/auth-form.ts
+++ src/app/auth-form/auth-form.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UserCredentials } from '../userInterface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './auth-form.html',
   styleUrl: './auth-form.scss',
 })
 export class AuthForm {
+  @Input() isLoginMode: boolean = true;
+  @Output() formSubmit = new EventEmitter<any>();
+  
+  submitted = false;
+  
+  credentials: UserCredentials = {
+    emailId: '',
+    fullName: '',
+    password: ''
+  };
 
+  onSubmit(form: any) {
+    this.submitted = true;
+    this.formSubmit.emit(form.value);
+  }
 }
